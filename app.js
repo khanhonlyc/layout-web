2f47e021bbc1a6653ae8315fde8794e9c470ba5c
khanhonlyc <80136458+khanhonlyc@users.noreply.github.com>
2023-05-27 09:25:41 +0700

    complete product list

---
+++ app.js
@@ -0,0 +1,13 @@
+$('.images-lider').slick(
+    {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        draggable: false,
+        prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    }
+);
